bd4f636479a84c9653ae5b077965049ee6c27fdb
Ivarock <ivarssluka@gmail.com>
2024-09-26 15:17:58 +0300

    forms and csrf

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,14 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @method static find($id)
+ * @method static create(string[] $array)
+ */
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = 'job_listings';
 
-    protected $fillable = [
-        'title',
-        'salary',
-    ];
+    protected $guarded = [];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
