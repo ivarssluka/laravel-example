6a545eff03a47b4b89f1171b5facb39bf388ddcd
Ivarock <ivarssluka@gmail.com>
2024-09-28 14:33:17 +0300

    Login and Registration system pt-1

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,25 +27,10 @@
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-4 flex items-center md:ml-6">
-                        <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span class="absolute -inset-1.5"></span>
-                            <span class="sr-only">View notifications</span>
-                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                            </svg>
-                        </button>
-
-                        <!-- Profile dropdown -->
-                        <div class="relative ml-3">
-                            <div>
-                                <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                    <span class="absolute -inset-1.5"></span>
-                                    <img class="h-8 w-8 rounded-full" src="https://imgs.search.brave.com/O_jjvMEiFomtjGZ9Ogakv6sPzW60FJY8FAd0AG1fZPw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzM2LzQ5Lzkw/LzM2MF9GXzEzNjQ5/OTA3N194cDdiU1FC/NER4MTNrdFFwME9Z/SjVyaWNXWGhpRnRE/Mi5qcGc" alt="">
-                                    <span class="sr-only">Open user menu</span>
-                                </button>
-                            </div>
-
-                        </div>
+                        @guest()
+                            <x-nav-link href="/login" :active="request()->is('login')">Log in</x-nav-link>
+                            <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+                        @endguest
                     </div>
                 </div>
                 <div class="-mr-2 flex md:hidden">
